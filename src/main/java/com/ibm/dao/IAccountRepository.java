bf2af615f10c80c94d98fe6898ca7af7ebc2dc18
harshitratan <harshitratanshukla@gmail.com>
2023-05-04 11:18:19 +0530

    Minor fix

--- src/main/java/com/ibm/dao/IAccountRepository.java
+++ src/main/java/com/ibm/dao/IAccountRepository.java
@@ -1,27 +1,11 @@
 package com.ibm.dao;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import com.ibm.bean.Account;
 
 @Component
-@Repository
 public interface IAccountRepository extends JpaRepository<Account, Integer>{
-
-	//void deleteBynum(int accnum);
-
-	//Optional<Account> findBynum(int accnum);
-	
-
 	
 }
-//	public Account addAccount(Account account);
-//	public Account removeAccount(long id);
-//	public Account updateAccount(long id, Account account);
-//	public Account getAccount(long id);
-//	public List<Account> getAllAccounts();
-//
--- src/main/java/com/ibm/dao/IStatementRepository.java
+++ src/main/java/com/ibm/dao/IStatementRepository.java
@@ -2,19 +2,10 @@ package com.ibm.dao;
 
 import com.ibm.bean.Statement;
 
-//import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-@Repository
+
 @Component
 public interface IStatementRepository extends JpaRepository<Statement, Long>{
-//	public Statement addStatement(Statement statement);
-//	public Statement removeStatement(long id);
-//	public Statement updateStatement(long id, Statement statement);
-//	public Statement getStatement(long id);
-//	public List<Statement> getAllStatements();
-//	public Statement getBilledStatement();
-//	public Statement getUnbilledStatement();
+	
 }
